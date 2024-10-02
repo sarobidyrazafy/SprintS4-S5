2d40162342e52e9af9a97b4662743d3c8f8f755e
Eloic Rakoto-Arisoa <99272320+Eloic1592@users.noreply.github.com>
2024-10-02 19:07:19 +0300

    Création de Restapi
    Une nouvelle classe annotation Restapi

---
+++ Framework/src/mg/itu/annotation/Restapi.java
@@ -0,0 +1,11 @@
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)  // Peut être appliquée aux classes et interfaces
+public @interface RestApi {
+    String url() default "";  // URL de base de l'API
+    String method() default "GET";  // Méthode HTTP par défaut
+}
