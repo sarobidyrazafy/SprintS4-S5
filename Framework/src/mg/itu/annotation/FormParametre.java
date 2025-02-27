666560073817ae6b85ff00be081737a1ffa7b497
Sarobidy Razafy <sarobidyraza101@gmail.com>
2025-02-27 21:35:00 +0300

    Sprint13-2616

--- Framework/src/mg/itu/annotation/FormParametre.java
+++
@@ -1,9 +0,0 @@
-package mg.itu.annotation;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.FIELD)
-@Retention(RetentionPolicy.RUNTIME)
-public @interface FormParametre {
-    String value();
-}
--- Framework/src/mg/itu/annotation/Parametre.java
+++
@@ -1,8 +0,0 @@
-package mg.itu.annotation;
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.PARAMETER)
-public @interface Parametre {
-    String name();
-}
